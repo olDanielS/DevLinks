59956389e7f5f96d2253dbeba6a27415441998b9
olDanielS <danielnrt80@gmail.com>
2022-11-11 12:33:49 -0300

    Not Found + Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import './home.css';
 
 export default function Home() {
  return (
-   <div>
-    <h1>Pagina de Home</h1>
+
+   <div className='home-container'>
+     <h1>Sujeito Programador</h1>
+     <span>Veja meus links &#128071;</span>
+    <main className='links'>
+      <section className='links-area'>
+          <a href='#'>
+            <p className='link-text'>Canal do Youtube</p>
+          </a>
+      </section>
+      <section className='links-area'>
+          <a href='#'>
+            <p className='link-text'>Instagram</p>
+          </a>
+      </section>
+      <section className='links-area'>
+          <a href='#'>
+            <p className='link-text'>Facebook</p>
+          </a>
+      </section>
+      <section className='links-area'>
+          <a href='#'>
+            <p className='link-text'>Twitter</p>
+          </a>
+      </section>
+    </main>
    </div>
  );
 }
